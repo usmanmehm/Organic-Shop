f658e73b2a158ac66e655d1cb57f1dc4ef502ac4
Usman Mehmood <usman.mehm@outlook.com>
2018-10-10 16:57:13 -0400

    Add main functionality + firebase login
    First commit:
    - includes navbar formatting
    - routes
    - firebase API
    - firebase login
    - logout option

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAwxo8Bt7ZeYfFLI5WrakbNGtJsskBv3wA",
-    authDomain: "oshop-f3445.firebaseapp.com",
-    databaseURL: "https://oshop-f3445.firebaseio.com",
-    projectId: "oshop-f3445",
-    storageBucket: "oshop-f3445.appspot.com",
-    messagingSenderId: "223112932947"    
+    apiKey: "AIzaSyDA4wGscVhJmWKbDJeFQdiJe04kvA75KFM",
+    authDomain: "oshop-a7f33.firebaseapp.com",
+    databaseURL: "https://oshop-a7f33.firebaseio.com",
+    projectId: "oshop-a7f33",
+    storageBucket: "oshop-a7f33.appspot.com",
+    messagingSenderId: "304734378527"  
   }
 };
